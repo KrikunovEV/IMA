3ccc1ea24d53b6ac194232b9cc67edd3e6c09d07
Jlevan25 <91471603+Jlevan25@users.noreply.github.com>
2021-12-15 22:41:08 +0300

    New logger - worker with commands

--- logger/__init__.py
+++ logger/__init__.py
@@ -1,4 +1,4 @@
-from logger.mlflow_logger import LoggerServer, RunLogger
+from logger.mlflow_logger import LoggerServer, RunLogger, Worker
 from logger.metric import IMetric, Metric, BatchMetric
 from logger.artifact import IArtifact, PickleArt, ModelArt
 from logger.cooperation import CooperationTask
