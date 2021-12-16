5891a2cbb8ef0cf6ec74eebd720c7aa69f4eb834
Jlevan25 <91471603+Jlevan25@users.noreply.github.com>
2021-12-16 16:07:54 +0300

    Add files via upload

---
+++ logger/commands.py
@@ -0,0 +1,66 @@
+from mlflow.utils.mlflow_tags import MLFLOW_RUN_NAME, MLFLOW_RUN_NOTE
+
+
+def init(worker, instance: str, run_name: str, metrics: tuple):
+    if instance in worker.instances:
+        raise Exception(f'Logger warning: two .init() calls are occurred.')
+
+    if len(metrics) == 0:
+        raise Exception(f'Logger: .init() has to input at least one \'metric\'.')
+
+    run_id = worker.logger.create_run(worker.exp_id, tags={MLFLOW_RUN_NAME: run_name}).info.run_id
+    worker.logger.set_tag(run_id, MLFLOW_RUN_NOTE, f'run_id: {run_id}')
+    for metric in metrics:
+        metric.set_mlflow(worker.logger, (worker.global_run_id if metric.is_global else run_id))
+    worker.instances[instance] = dict(run_id=run_id, metrics=metrics)
+
+
+def log(worker, instance: str, data: dict):
+    for key, value in data.items():
+        for metric in worker.instances[instance]['metrics']:
+            if metric.is_able_to_log() and metric.key == key:
+                metric.on_log(value)
+
+
+def set_mode(worker, instance: str, mode: bool):
+    for metric in worker.instances[instance]['metrics']:
+        metric.set_mode(mode)
+
+
+def on_all(worker, instance: str):
+    for metric in worker.instances[instance]['metrics']:
+        if hasattr(metric, 'on_all'):
+            metric.on_all()
+
+
+def call(worker, instance: str, func_name: str, args):
+    for metric in worker.instances[instance]['metrics']:
+        if hasattr(metric, f'{func_name}'):
+            getattr(metric, f'{func_name}')(args)
+
+
+def param(worker, instance: str, data: dict):
+    for key, value in data.items():
+        worker.logger.log_param(worker.instances[instance]['run_id'], key, value)
+
+
+def artifact(worker, instance: str, src_path: str, dest_dir: str):
+    worker.logger.log_artifact(run_id=worker.instances[instance]['run_id'], local_path=src_path, artifact_path=dest_dir)
+
+
+def artifacts(worker, instance: str, src_path: str, dest_dir: str):
+    worker.logger.log_artifacts(run_id=worker.instances[instance]['run_id'], local_dir=src_path, artifact_path=dest_dir)
+
+
+def deinit(worker, instance: str):
+    if instance in worker.instances:
+        worker.logger.set_terminated(worker.instances[instance]['run_id'], worker.terminated)
+        worker.instances.pop(instance)
+
+
+def stop(worker, instance: str):
+    if instance == 'server':
+        worker.logger.set_terminated(worker.global_run_id, worker.terminated)
+        return 'stop'
+    else:
+        raise Exception(f"Logger: command.stop() only for 'server', but was '{instance}'.")
--- logger/mlflow_logger.py
+++ logger/mlflow_logger.py
@@ -1,30 +1,14 @@
 from mlflow.tracking import MlflowClient
 from mlflow.entities import RunStatus
 from mlflow.utils.mlflow_tags import MLFLOW_RUN_NAME, MLFLOW_RUN_NOTE
-
 import multiprocessing as mp
-from enum import Enum, unique
-from typing import Any
 
+from logger import commands
 from logger.metric import IMetric
 
 
-@unique
-class CMD(Enum):
-    INIT = 'init'
-    LOG = 'log'
-    STOP = 'stop'
-    MODE = 'set_mode'
-    ALL = 'all'
-    CALL = 'call'
-    PARAM = 'param'
-    ART = 'artifact'
-    ARTS = 'artifacts'
-    DEINIT = 'deinit'
-
-
-def _send(queue: mp.Queue, instance: str, cmd: CMD, data: Any):
-    queue.put((instance, cmd, data))
+def _send(queue: mp.Queue, command, instance, *args, **kwargs):
+    queue.put((command, instance, args, kwargs))
 
 
 def load_model_from_artifacts(exp_id: str, run_id: str, state_dict_path: str):
@@ -37,94 +21,42 @@ def load_model(path: str):
     return torch.load(path)
 
 
+class Worker:
+    def __init__(self, logger, experiment_name):
+        self.logger = logger
+        self.instances = {}
+        self.exp_id = self._find_experiment_id(experiment_name)
+        self.global_run_id = logger.create_run(self.exp_id, tags={MLFLOW_RUN_NAME: 'global'}).info.run_id
+        self.terminated = RunStatus.to_string(RunStatus.FINISHED)
+
+    def _find_experiment_id(self, experiment_name):
+        _exp = self.logger.get_experiment_by_name(experiment_name)
+        if _exp is not None:
+            return _exp.experiment_id
+        else:
+            _exp_count = 0
+            while True:
+                _exp_name = experiment_name + str(_exp_count)
+                _exp = self.logger.get_experiment_by_name(_exp_name)
+                _exp_count += 1
+                if _exp is None:
+                    return self.logger.create_experiment(_exp_name)
+
+    def set_tag(self, run_id):
+        self.logger.set_tag(run_id, MLFLOW_RUN_NOTE, f'run_id: {run_id}')
+
+
 def _mlflow_worker(message_queue: mp.Queue, experiment_name: str):
-    # initialize mlflow client
-    logger = MlflowClient()
-
-    # try to create unique experiment if experiment_name does not exist otherwise continue
-    _exp = logger.get_experiment_by_name(experiment_name)
-    if _exp is not None:
-        exp_id = _exp.experiment_id
-    else:
-        _exp_count = 0
-        while True:
-            _exp_name = experiment_name + str(_exp_count)
-            _exp = logger.get_experiment_by_name(_exp_name)
-            _exp_count += 1
-            if _exp is None:
-                exp_id = logger.create_experiment(_exp_name)
-                break
-
-    # initialize global run
-    global_run_id = logger.create_run(exp_id, tags={MLFLOW_RUN_NAME: 'global'}).info.run_id
-    logger.set_tag(global_run_id, MLFLOW_RUN_NOTE, f'run_id: {global_run_id}')
-
-    # other meta information
-    instances = dict()
-    terminated = RunStatus.to_string(RunStatus.FINISHED)
+    worker = Worker(logger=MlflowClient(), experiment_name=experiment_name)
 
     while True:
-        instance, cmd, data = message_queue.get()
+        cmd_func, instance, args, kwargs = message_queue.get()
 
-        if instance not in instances and cmd != CMD.INIT and cmd != CMD.STOP:
-            raise Exception(f'Logger: trying to .{cmd.value}(), but .{CMD.INIT.value}() was not called yet.')
+        if instance is None:
+            raise Exception(f'Logger: init() was not called yet.')
 
-        if cmd == CMD.INIT:
-            # data: tuple (str run_name, IMetric args)
-            if instance in instances:
-                raise Exception(f'Logger warning: two .{CMD.INIT.value}() calls are occurred.')
-            else:
-                metrics = list(data[1])
-                if len(metrics) > 0:
-                    run_id = logger.create_run(exp_id, tags={MLFLOW_RUN_NAME: data[0]}).info.run_id
-                    logger.set_tag(run_id, MLFLOW_RUN_NOTE, f'run_id: {run_id}')
-                    for metric in metrics:
-                        if metric.is_global:
-                            metric.set_mlflow(logger, global_run_id)
-                        else:
-                            metric.set_mlflow(logger, run_id)
-                    instances[instance] = dict(run_id=run_id, metrics=metrics)
-                else:
-                    raise Exception(f'Logger: .{CMD.INIT}() has to input at least one \'metric\'.')
-        elif cmd == CMD.LOG:
-            # data: dict data
-            for key, value in data.items():
-                for metric in instances[instance]['metrics']:
-                    if metric.is_able_to_log() and metric.key == key:
-                        metric.on_log(value)
-        elif cmd == CMD.MODE:
-            # data: bool train
-            for metric in instances[instance]['metrics']:
-                metric.set_mode(data)
-        elif cmd == CMD.ALL:
-            # data: None
-            for metric in instances[instance]['metrics']:
-                if hasattr(metric, 'on_all'):
-                    metric.on_all()
-        elif cmd == CMD.CALL:
-            # data: tuple (str func, data)
-            for metric in instances[instance]['metrics']:
-                if hasattr(metric, f'{data[0]}'):
-                    getattr(metric, f'{data[0]}')(data[1])
-        elif cmd == CMD.PARAM:
-            # data: dict params
-            for key, value in data.items():
-                logger.log_param(instances[instance]['run_id'], key, value)
-        elif cmd == CMD.ART:
-            # data: tuple (str src_path, str dest_path)
-            logger.log_artifact(run_id=instances[instance]['run_id'], local_path=data[0], artifact_path=data[1])
-        elif cmd == CMD.ARTS:
-            # data: tuple (str src_path, str dest_path)
-            logger.log_artifacts(run_id=instances[instance]['run_id'], local_dir=data[0], artifact_path=data[1])
-        elif cmd == CMD.STOP:
-            # data: None
-            logger.set_terminated(global_run_id, terminated)
+        if cmd_func(worker, instance, *args, **kwargs):
             break
-        elif cmd == CMD.DEINIT:
-            # data: None
-            if instance in instances:
-                logger.set_terminated(instances[instance]['run_id'], terminated)
-                instances.pop(instance)
 
 
 class RunLogger:
@@ -132,36 +64,38 @@ class RunLogger:
 
     def __init__(self, queue: mp.Queue):
         self._queue = queue
-        self._instance = f'{mp.current_process().name}_{RunLogger.INSTANCE_COUNT}'
+        self._instance = None
+        self._id = RunLogger.INSTANCE_COUNT
         RunLogger.INSTANCE_COUNT += 1
 
     def init(self, run_name: str, train: bool, *args: IMetric):
-        _send(self._queue, self._instance, CMD.INIT, (run_name, args))
+        self._instance = f'{mp.current_process().name}_{self._id}'
+        _send(self._queue, commands.init, self._instance, run_name, args)
         self.set_mode(train)
 
     def set_mode(self, train: bool):
-        _send(self._queue, self._instance, CMD.MODE, train)
+        _send(self._queue, commands.set_mode, self._instance, train)
 
     def log(self, data: dict):
-        _send(self._queue, self._instance, CMD.LOG, data)
+        _send(self._queue, commands.log, self._instance, data)
 
     def all(self):
-        _send(self._queue, self._instance, CMD.ALL, None)
+        _send(self._queue, commands.on_all, self._instance)
 
     def call(self, func: str, data):
-        _send(self._queue, self._instance, CMD.CALL, (func, data))
+        _send(self._queue, commands.call, self._instance, func, data)
 
     def param(self, params: dict):
-        _send(self._queue, self._instance, CMD.PARAM, params)
+        _send(self._queue, commands.param, self._instance, params)
 
     def artifact(self, src_path: str, dest_dir: str = None):
-        _send(self._queue, self._instance, CMD.ART, (src_path, dest_dir))
+        _send(self._queue, commands.artifact, self._instance, src_path, dest_dir)
 
     def artifacts(self, src_path: str, dest_dir: str = None):
-        _send(self._queue, self._instance, CMD.ARTS, (src_path, dest_dir))
+        _send(self._queue, commands.artifacts, self._instance, src_path, dest_dir)
 
     def deinit(self):
-        _send(self._queue, self._instance, CMD.DEINIT, None)
+        _send(self._queue, commands.deinit, self._instance)
 
 
 class LoggerServer:
@@ -178,7 +112,7 @@ class LoggerServer:
         self._process.start()
 
     def stop(self):
-        _send(self._queue, self._instance, CMD.STOP, None)
+        _send(self._queue, commands.stop, self._instance)
         self._process.join()
 
     @property
